3a5f17d62c856d51c4ffd743a3e049be44f2f7af
abcd <nishant@netforceinfotech.com>
2016-08-25 11:00:20 +0530

    new upades

---
+++ app/src/main/java/com/netforceinfotech/todo_tobuy/Login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.netforceinfotech.todo_tobuy.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.netforceinfotech.todo_tobuy.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
--- app/src/main/java/com/netforceinfotech/todo_tobuy/MainActivity.java
+++ app/src/main/java/com/netforceinfotech/todo_tobuy/MainActivity.java
@@ -1,7 +1,19 @@
 package com.netforceinfotech.todo_tobuy;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.netforceinfotech.todo_tobuy.Login.LoginActivity;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // do something
+                try {
+                    PackageInfo info = getPackageManager().getPackageInfo(
+                            "com.netforceinfotech.elipseexpress",
+                            PackageManager.GET_SIGNATURES);
+                    for (Signature signature : info.signatures) {
+                        MessageDigest md = MessageDigest.getInstance("SHA");
+                        md.update(signature.toByteArray());
+                        Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+
+                } catch (NoSuchAlgorithmException e) {
+
+                }
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                overridePendingTransition(R.anim.enter, R.anim.exit);
+                finish();
+            }
+        }, 2000);
+    }
     }
 }
